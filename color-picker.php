b2dd11cca3858db120f41dd52a60e38e17027c2d
Mohammad Taufan Pramono <pramonotaufan@gmail.com>
2023-12-29 09:19:00 +0700

    Create color-picker.php

---
+++ color-picker.php
@@ -0,0 +1,98 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <style>
+    #colorPicker {
+      cursor: crosshair;
+      position: relative;
+    }
+  </style>
+  <title>Color Picker</title>
+</head>
+<body>
+
+  <h1>Color Picker</h1>
+  <p>Klik pada gambar atau unggah gambar untuk memilih warna.</p>
+  <input type="file" id="imageInput" accept="image/*">
+  <canvas id="colorPicker" width="300" height="200"></canvas>
+  <p id="selectedColor"></p>
+  <div id="captureList"></div>
+
+  <script>
+    document.addEventListener("DOMContentLoaded", function() {
+      var canvas = document.getElementById("colorPicker");
+      var ctx = canvas.getContext("2d");
+      var selectedColorElement = document.getElementById("selectedColor");
+      var imageInput = document.getElementById("imageInput");
+      var captureList = document.getElementById("captureList");
+      var captureNumber = 1;
+
+      imageInput.addEventListener("change", function(event) {
+        var file = event.target.files[0];
+        if (file) {
+          var reader = new FileReader();
+          reader.onload = function(e) {
+            var image = new Image();
+            image.src = e.target.result;
+            image.onload = function() {
+              ctx.drawImage(image, 0, 0, canvas.width, canvas.height);
+            };
+          };
+          reader.readAsDataURL(file);
+        }
+      });
+
+      canvas.addEventListener("click", function(event) {
+        var x = event.clientX - canvas.getBoundingClientRect().left;
+        var y = event.clientY - canvas.getBoundingClientRect().top;
+
+        // Mendapatkan nilai warna pada titik yang diklik
+        var pixel = ctx.getImageData(x, y, 1, 1).data;
+        var rgb = "RGB(" + pixel[0] + ", " + pixel[1] + ", " + pixel[2] + ")";
+        selectedColorElement.textContent = "Warna terpilih: " + rgb;
+
+        // Menambahkan fitur lingkaran
+        var radius = 10; // Radius lingkaran bisa disesuaikan sesuai kebutuhan
+        drawCircle(x, y, radius);
+        showSelectedColorsInCircle(x, y, radius);
+
+        // Menambahkan informasi capture ke dalam daftar
+        var captureInfo = document.createElement("p");
+        captureInfo.textContent = captureNumber + ". " + rgb + " Capture " + captureNumber;
+        captureList.appendChild(captureInfo);
+
+        // Menambahkan nomor capture
+        captureNumber++;
+      });
+
+      // Fungsi untuk menggambar lingkaran di posisi tertentu
+      function drawCircle(x, y, radius) {
+        ctx.beginPath();
+        ctx.arc(x, y, radius, 0, 2 * Math.PI);
+        ctx.strokeStyle = "#000000"; // Warna lingkaran (hitam)
+        ctx.lineWidth = 2;
+        ctx.stroke();
+      }
+
+      // Fungsi untuk menampilkan seluruh data RGB yang terseleksi di dalam lingkaran
+      function showSelectedColorsInCircle(x, y, radius) {
+        var selectedColors = [];
+
+        for (var i = x - radius; i <= x + radius; i++) {
+          for (var j = y - radius; j <= y + radius; j++) {
+            var pixel = ctx.getImageData(i, j, 1, 1).data;
+            var rgb = "RGB(" + pixel[0] + ", " + pixel[1] + ", " + pixel[2] + ")";
+            selectedColors.push(rgb);
+          }
+        }
+
+        console.log("Seluruh data RGB di dalam lingkaran:", selectedColors);
+        // Anda dapat menampilkan data RGB ini di bagian UI atau tempat lain sesuai kebutuhan
+      }
+    });
+  </script>
+
+</body>
+</html>
